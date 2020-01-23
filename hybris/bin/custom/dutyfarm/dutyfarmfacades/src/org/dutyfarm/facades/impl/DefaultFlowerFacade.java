73e6590ec9de12bef48e5b447d4d865c6c04f209
Natalia Albanese <natalia.albanese@soprasteria.com>
2020-01-23 18:26:03 +0100

    modify bean

--- hybris/bin/custom/dutyfarm/dutyfarmfacades/src/org/dutyfarm/facades/impl/DefaultFlowerFacade.java
+++ hybris/bin/custom/dutyfarm/dutyfarmfacades/src/org/dutyfarm/facades/impl/DefaultFlowerFacade.java
@@ -1,6 +1,9 @@
 package org.dutyfarm.facades.impl;
 
+import org.apache.log4j.Logger;
 import org.dutyfarm.facades.FlowerFacade;
 
 public class DefaultFlowerFacade implements FlowerFacade {
+
+    private static final Logger LOG = Logger.getLogger(DefaultFlowerFacade.class);
 }
--- hybris/bin/custom/dutyfarm/dutyfarmfacades/src/org/dutyfarm/facades/impl/DefaultFlowerVariantFacade.java
+++ hybris/bin/custom/dutyfarm/dutyfarmfacades/src/org/dutyfarm/facades/impl/DefaultFlowerVariantFacade.java
@@ -1,6 +1,10 @@
 package org.dutyfarm.facades.impl;
 
+import org.apache.log4j.Logger;
 import org.dutyfarm.facades.FlowerVariantFacade;
 
 public class DefaultFlowerVariantFacade implements FlowerVariantFacade {
+
+    private static final Logger LOG = Logger.getLogger(DefaultFlowerVariantFacade.class);
+
 }
