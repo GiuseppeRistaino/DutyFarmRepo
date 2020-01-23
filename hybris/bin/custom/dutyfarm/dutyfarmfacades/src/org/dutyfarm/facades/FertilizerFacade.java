0d50daeba91b0b6773bfdeb6ee597f9cc0bb5cde
RISTAINO Giuseppe <giuseppe.ristaino@soprasteria.com>
2020-01-23 17:14:14 +0100

    update bean Data

--- hybris/bin/custom/dutyfarm/dutyfarmfacades/src/org/dutyfarm/facades/FertilizerFacade.java
+++ hybris/bin/custom/dutyfarm/dutyfarmfacades/src/org/dutyfarm/facades/FertilizerFacade.java
@@ -1,4 +1,11 @@
 package org.dutyfarm.facades;
 
+import org.dutyfarm.core.model.FertilizerProductModel;
+
+import java.util.List;
+
 public interface FertilizerFacade {
+
+    //public List<FertilizerProductData> getAllFertilizers();
+
 }
